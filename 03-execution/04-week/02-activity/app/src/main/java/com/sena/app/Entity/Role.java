67d66c2ff9b8a839b686ab8808122cf07021c7d3
sharikrojas56 <sharikrojas.sena@gmail.com>
2026-03-09 14:21:53 -0500

    App

---
+++ 03-execution/04-week/02-activity/app/src/main/java/com/sena/app/Entity/Role.java
@@ -0,0 +1,40 @@
+// ROLE
+
+package com.sena.app.Entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name", nullable=false, length=100)
+    private String name;
+
+    @Column(name="description", length=255)
+    private String description;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
